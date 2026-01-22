cac54e5b5ea26d611f15a72137e9ab206da12557
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-01-22 10:07:04 -0500

    add new sketch.

---
+++ sketches/260122/sketch.js
@@ -0,0 +1,10 @@
+//unnamed; month, year. 
+
+function setup() {
+  // createCanvas(1000, 562); //in 16:9 aspect ratio.
+  createCanvas(800, 800); //square to handle calculations better. 
+}
+
+function draw() {
+background(0);
+}
