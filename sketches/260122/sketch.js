5eb76f228a2f58dfb9e0f5b2c6d625254fcdd96a
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-01-24 17:56:13 -0500

    get transformations right.

--- sketches/260122/sketch.js
+++ sketches/260122/sketch.js
@@ -14,17 +14,12 @@ an enclosed body can be thought of as a group of similar colours.
 
 let cam;
 
-let vertices = [];
-let pixelation = 20;
-
-let col_similarity_threshold = 50;
-
 function setup() {
   cam = createCapture(VIDEO, make_canvas);
   cam.hide();
 }
 
-//helper to convert canvas to aspect ratio. runs once.
+//make canvas the same size as camera for easier calculations.
 function make_canvas() {
   createCanvas(cam.width, cam.height, WEBGL);
 }
@@ -32,61 +27,16 @@ function make_canvas() {
 function draw() {
   background(0);
   push();
-  //first flip
-  scale(-1, 1);
-  translate(-width, 0);
-
-  translate(width / 2, -height / 2); //bring back to the center.
-
-  assign_vertices();
-
-  //draw vertices to canvas:
-
-  for (let sing_vertex of vertices) {
-    beginShape(POINTS);
-    sing_vertex.x = map(sing_vertex.x, 0, cam.width, 0, width);
-    sing_vertex.y = map(sing_vertex.y, 0, cam.height, 0, height);
-
-    stroke(sing_vertex.r, sing_vertex.g, sing_vertex.b);
-    fill(sing_vertex.r, sing_vertex.g, sing_vertex.b);
-
-    vertex(sing_vertex.x, sing_vertex.y);
-    endShape();
-  }
-
-  //image(cam, 0, 0, width, height);
+  //transformations are additive. first, to get origin to top-left, we subtract 
+  translate(-width/2, -height / 2); //bring back to the center.
+    push ();
+    scale (-1,1); 
+    translate (-width, 0); 
+    image(cam, 0, 0, width, height);
+    pop(); 
+  fill(255);
+  rect(0, 0, 100, 100);
   pop();
-}
-
-function assign_vertices() {
-  cam.loadPixels();
-  //now we get a cam.pixels array with rgba values for each pixel.
-
-  /*
-  algorithm: 
-  - we go through every single x, y position on the screen. 
-  - for each pixel, look at its neighbours (8).
-  - if the colour of the neighbour is similar (under a certain threshold), accumulate them into an object. 
-  - if the neighbour has been accumulated, don't look at it again. 
-  */
-
-  for (let x = 0; x < cam.width; x += pixelation) {
-    for (let y = 0; y < cam.height; y += pixelation) {
-      let n = get_pixel_index(x, y);
-
-      let r = cam.pixels[n];
-      let g = cam.pixels[n + 1];
-      let b = cam.pixels[n + 2];
-
-      vertices.push({
-        x,
-        y,
-        r,
-        g,
-        b,
-      });
-    }
-  }
 }
 
 //helper to convert x,y coordinates to pixels index.
