3c6a34177f43c247240680fec01770366b7bd3e1
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-01-26 13:20:31 -0500

    gpted; working.

--- sketches/260122/sketch.js
+++ sketches/260122/sketch.js
@@ -30,57 +30,117 @@ function make_canvas() {
 function draw() {
   background(0);
 
-  push();
-  translate(-width / 2, -height / 2);
-  //webgl renders canvas from the center of the screen; so.
-  image(cam, 0, 0, width, height);
-  pop();
+//   push();
+//   translate(-width / 2, -height / 2);
+//   //webgl renders canvas from the center of the screen; so.
+//   image(cam, 0, 0, width, height);
+//   pop();
 
   make_groups();
 
-  console.log(get_neighbours(0, 0));
+  draw_groups();
 }
 
 //this function will read all colour values, cluster similar colours together into a group.
-// this'll be in an array groups[n][e], where [n] is the number of groups, and [e] is the number of pixels it has clustered together.
 
-let groups;
+// we keep two variables: 
+let groups; //stores data in the format: groups[id][indices], where id increments sequentially, and indices is a list of all cam.pixels indices that belong to the group. 
+
+let pixel_to_group;//this stores data in this format: studied_index: groupid. we use this to check if the pixel being studied has been grouped or not. 
 
 function make_groups() {
   cam.loadPixels();
   //^ returns a cam.pixels array with rgba information.
 
+  // reset every frame
+  groups = [];
+  pixel_to_group = {};
+
   // go through every single pixel and compare its colour value with its neighbour.
-
-  let group_numbers = 0;
-
   for (let x = 0; x < cam.width; x += pixelation) {
     for (let y = 0; y < cam.height; y += pixelation) {
+      // this is the studied pixel. we get its values first.
+      let studied_index = get_pixel_index(x, y);
 
-      let neighbours = get_neighbours(x, y);
+      let studied_rgba = color(cam.pixels[studied_index], cam.pixels[studied_index + 1], cam.pixels[studied_index + 2], cam.pixels[studied_index + 3]);
 
-      let n = get_pixel_index(x, y);
-
-      //study calculations. we study hue.
-      let studied_rgba = color(cam.pixels[n], cam.pixels[n + 1], cam.pixels[n + 2], cam.pixels[n + 3]);
       let studied_hue = Math.floor(hue(studied_rgba));
 
-      //now we compare it to every neighbour.
-      for (let i = 0; i < neighbours.length; i++) {
-        // see if it has been grouped previously or not. 
-        
-        let neighbour_rgba = color(cam.pixels[i], cam.pixels[i + 1], cam.pixels[i + 2], cam.pixels[i + 3]);
-        let studied_hue = Math.floor(hue(neighbour_rgba));
+      // now we have the hue of the pixel being looked at. we now want to compare it to its neighbours.
+      let neighbours = get_neighbours(x, y);
+      // ^ returns an array of indices for cam.pixels.
 
-        if (neighbour_hue >= studied_hue - similarity_threshold && neighbour_hue <= studied_hue + similarity_threshold) {
-          //this is a valid colour to be grouped.
-        } else {
+      // this will store the group id if we find a matching neighbour.
+      let found_group = null;
+
+      // now we compare the studied hue to neighbour_hue.
+      for (let i = 0; i < neighbours.length; i++) {
+        let neighbour_index = neighbours[i];
+
+        // first see if the neighbour being studied has been already sent to a group or not. 
+        if (pixel_to_group[neighbour_index] === undefined) {
           continue;
         }
+
+        let neighbour_rgba = color(cam.pixels[neighbour_index], cam.pixels[neighbour_index + 1], cam.pixels[neighbour_index + 2], cam.pixels[neighbour_index + 3]);
+
+        let neighbour_hue = Math.floor(hue(neighbour_rgba));
+
+        // study calculations. we study hue.
+        if (abs(neighbour_hue - studied_hue) < similarity_threshold) {
+          // this is a valid colour to be grouped.
+          found_group = pixel_to_group[neighbour_index];
+          break; // first valid group is enough
+        }
+      }
+
+      // if no neighbour matched, create a new group
+      if (found_group === null) {
+        let new_group_id = groups.length;
+        groups.push([studied_index]);
+        pixel_to_group[studied_index] = new_group_id;
+      }
+      // otherwise, add this pixel to the existing group
+      else {
+        groups[found_group].push(studied_index);
+        pixel_to_group[studied_index] = found_group;
       }
     }
   }
+
+  console.log (groups, pixel_to_group); 
 }
+
+function draw_groups() {
+  push();
+
+  // WEBGL canvas is centered; match image space
+  translate(-width / 2, -height / 2);
+
+  noStroke();
+  colorMode(RGB, 255);
+
+  for (let g = 0; g < groups.length; g++) {
+    for (let i = 0; i < groups[g].length; i++) {
+      let index = groups[g][i];
+      let { x, y } = index_to_xy(index);
+
+      let r = cam.pixels[index];
+      let g_ = cam.pixels[index + 1];
+      let b = cam.pixels[index + 2];
+      let a = cam.pixels[index + 3];
+
+      fill(r, g_, b, a);
+      circle(x, y, pixelation * 0.8);
+    }
+  }
+
+  pop();
+}
+  
+
+
+/* helpers: */
 
 //helper written by an llm to fetch neighbours for a given pixel, taking in mind the pixelation value.
 function get_neighbours(x, y) {
@@ -120,4 +180,16 @@ function get_neighbours(x, y) {
 //helper to convert x,y coordinates to pixels index.
 function get_pixel_index(x, y) {
   return (y * cam.width + x) * 4;
+}
+
+//helper to convert index to coordinates.
+function index_to_xy(index) {
+  let p = index / 4;
+  let x = p % cam.width;
+  let y = Math.floor(p / cam.width);
+  return { x, y };
+}
+
+function mousePressed(){
+    noLoop(); 
 }
