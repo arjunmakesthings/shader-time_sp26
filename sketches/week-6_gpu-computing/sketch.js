972c6b48f314623cd180b6fd4deaeb875ad14ec1
arjun <82059571+arjunmakesthings@users.noreply.github.com>
2026-03-08 08:33:35 -0400

    base.

--- sketches/week-6_gpu-computing/sketch.js
+++ sketches/week-6_gpu-computing/sketch.js
@@ -12,14 +12,20 @@ thought:
 
 let main_shader;
 let compute_shader;
+let compute_shader_2;
 
 let compute_buffer;
 
+let buffer_2;
+
 let m_coords = [-1000, -1000]; //set to offscreen.
+
+let tog = false;
 
 function preload() {
   main_shader = loadShader("vert.vert", "frag.frag");
   compute_shader = loadShader("vert.vert", "compute.frag");
+  compute_shader_2 = loadShader("vert.vert", "compute.frag");
 }
 
 function setup() {
@@ -29,45 +35,61 @@ function setup() {
   noStroke();
 
   compute_buffer = createGraphics(width, height, WEBGL); //a canvas that you never draw, but use to compute.
+
+  buffer_2 = createGraphics(width, height, WEBGL); //second buffer to ping-pong to.
 }
 
-let init_x = 50; 
-let init_y = 50; 
+let init_x = 50;
+let init_y = 50;
 
-let target_x = 800-50; 
-let target_y = 800 - 50; 
+let target_x = 800 - 50;
+let target_y = 800 - 50;
+
+let buffers = [];
 
 function draw() {
-  background(255);
+  // background(255);
 
-  //compute shader:
-  compute_buffer.shader(compute_shader);
-  compute_shader.setUniform("u_prev", compute_buffer);
-  compute_shader.setUniform("u_mouse", m_coords);
-  compute_shader.setUniform("u_res", [width, height]);
-  compute_buffer.rect(0, 0, width, height);
+  if (tog) {
+    buffer_2.shader(compute_shader_2);
+    compute_shader_2.setUniform("u_prev", compute_buffer);
+    compute_shader_2.setUniform("u_mouse", m_coords);
+    compute_shader_2.setUniform("u_res", [width, height]);
+    buffer_2.rect(0, 0, width, height);
+  } else {
+    compute_buffer.shader(compute_shader);
+    compute_shader.setUniform("u_prev", buffer_2);
+    compute_shader.setUniform("u_mouse", m_coords);
+    compute_shader.setUniform("u_res", [width, height]);
+    compute_buffer.rect(0, 0, width, height);
+  }
 
-  //main shader.
+  let current = tog ? buffer_2 : compute_buffer;
+
   shader(main_shader);
-
-  //uniforms for the main buffer:
-  main_shader.setUniform("u_map", compute_buffer);
+  main_shader.setUniform("u_map", current);
   main_shader.setUniform("u_res", [width, height]);
   rect(0, 0, width, height);
 
+  tog = !tog;
+
   //make it repeat over time.
-  
-  if (frameCount % 300 === 0) {
-    init_x = target_x;
-    init_y = target_y;
 
-    target_x = random(50, width - 50);
-    target_y = random(50, height - 50);
-  }
+  // if (frameCount % 300 === 0) {
+  //   init_x = target_x;
+  //   init_y = target_y;
 
-  let t = (frameCount % 300) / 300.0;
-  let x = lerp(init_x, target_x, t);
-  let y = lerp(init_y, target_y, t);
+  //   target_x = random(50, width - 50);
+  //   target_y = random(50, height - 50);
+  // }
 
-  m_coords = [x, y];
+  // let t = (frameCount % 300) / 300.0;
+  // let x = lerp(init_x, target_x, t);
+  // let y = lerp(init_y, target_y, t);
+
+  // m_coords = [x, y];
+}
+
+function mousePressed() {
+  m_coords = [mouseX, mouseY];
 }
